c9bffa518e85c6474ba22019a2e8d579f2c48a16
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-21 17:48:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,20 @@
+=begin
+using the .each method to reverse each word
 def reverse_each_word(string)
-  #using the .each method for reversing each word
-	#reversed_string = string.split(" ")
-	#new_str_array = []
-	#new_str_obj = ""
-	#counter = 0
-	#while reversed_string.length > counter do
-	#	reversed_string.each do |new_str|
-	#		new_str_obj = new_str.reverse
-	#		new_str_array.push(new_str_obj)
-	#		counter += 1
-	#	end
-	#end
-	#new_str_array.join(" ")
-	
-	#using the .collect method to reverse each word
-	
-	
-	
+  using the .each method for reversing each word
+	reversed_string = string.split(" ")
+	new_str_array = []
+	new_str_obj = ""
+	counter = 0
+	while reversed_string.length > counter do
+		reversed_string.each do |new_str|
+			new_str_obj = new_str.reverse
+			new_str_array.push(new_str_obj)
+			counter += 1
+		end
+	end
+	new_str_array.join(" ")
 end
+=end
+
+#using the .collect method to achieve the same result
