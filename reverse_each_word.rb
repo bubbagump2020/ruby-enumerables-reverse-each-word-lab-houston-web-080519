028cf7c8b3fb29a589441421039509fdd3934e7e
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-21 17:56:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -21,3 +21,21 @@ end
 =end
 
 #using the .collect method to achieve the same result
+
+def reverse_each_word(string)
+
+	reversed_string = string.split(" ")
+	new_str_array = []
+	new_str_obj = ""
+	counter = 0
+
+	while reversed_string.length > counter do
+		reversed_string.collect do |new_str|
+			new_str_obj = new_str.reverse
+			new_str_array.push(new_str_obj)
+			counter += 1
+		end
+	end
+	new_str_array.join(" ")
+
+end
